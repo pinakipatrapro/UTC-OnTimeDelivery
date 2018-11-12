521ddff82c85bb6e761e3876975bbc1bff9555e7
Pinaki Patra <pinaki.patra.pro@gmail.com>
2018-11-12 15:44:57 +0530

    Add Image  as response

--- bot.js
+++ bot.js
@@ -1,7 +1,7 @@
-
+var Fuse = require('fuse.js');
 const { ActivityTypes, CardFactory } = require('botbuilder');
 const { LuisRecognizer } = require('botbuilder-ai');
-const { DialogSet, DialogTurnStatus,TextPrompt } = require('botbuilder-dialogs');
+const { DialogSet, DialogTurnStatus, TextPrompt } = require('botbuilder-dialogs');
 const { IntentRouter } = require('./util/intentRouter');
 var intentRouter = new IntentRouter();
 
@@ -9,11 +9,11 @@ const { serviceMetadata } = require('./backendModel/metadata');
 
 const DIALOG_STATE_PROPERTY = 'dialogState';
 const USER_PROFILE_PROPERTY = 'userProfileProperty';
- 
+
 const LUIS_CONFIGURATION = 'UTC-VA-Query-Parser';
- 
+
 class BasicBot {
-  
+
   constructor(conversationState, userState, botConfig) {
 
     const luisConfig = botConfig.findServiceByNameOrId(LUIS_CONFIGURATION);
@@ -33,8 +33,8 @@ class BasicBot {
     this.conversationState = conversationState;
     this.userState = userState;
 
-    this.queryState  = {
-      status : 'new'
+    this.queryState = {
+      status: 'new'
     }
   }
 
@@ -42,20 +42,59 @@ class BasicBot {
   async onTurn(context) {
 
     if (context.activity.type === ActivityTypes.Message) {
-      let dialogResult;
-      const dc = await this.dialogs.createContext(context);
+      let dialogResult; 
 
       const results = await this.luisRecognizer.recognize(context);
       const topIntent = LuisRecognizer.topIntent(results);
+
       
-      var fetchProduct = await intentRouter.fetchProducts(context);
-      await context.sendActivity(fetchProduct);
+      // await context.sendActivity(this.parseMetadata(results,topIntent,context));
+      await this.parseMetadata(results,topIntent,context)
 
     }
     this.queryState.status = Math.random();
     await this.conversationState.saveChanges(context);
     await this.userState.saveChanges(context);
   }
+
+  async parseMetadata(results,topIntent,context){
+    
+    var aMeasures = results.entities.measure;
+    var aDimension = results.entities.dim;
+    if(topIntent === 'measurebydimension'){
+      var result = await this.parseIntentmeasurebydimension(aMeasures[0],aDimension[0],context);
+    }
+    
+  }
+
+  async parseIntentmeasurebydimension(measure,dimension,context){
+    var options = {
+      shouldSort: true,
+      threshold: 0.3,
+      includeScore: true,
+      location: 0,
+      distance: 100,
+      maxPatternLength: 32,
+      minMatchCharLength: 1,
+      keys: [
+        "_sap:label"
+    ]
+    };
+    var formattedDimension = this.fuseSearch(serviceMetadata.Property, options, dimension);
+    var formattedMeasure   = this.fuseSearch(serviceMetadata.Property, options, measure);
+
+    var result = await intentRouter.measurebydimension(formattedMeasure,formattedDimension);
+    await context.sendActivity(result);
+  }
+
+  fuseSearch(list,options,searchString){
+    var fuse = new Fuse(serviceMetadata.Property, options);  
+    var result = fuse.search(searchString);
+    
+    return result[0].item;
+  }
+
+
 
 
 
--- util/intentRouter.js
+++ util/intentRouter.js
@@ -1,5 +1,5 @@
 var request = require('request');
-const { ActivityTypes, MessageFactory, TurnContext } = require('botbuilder');
+const { ActivityTypes, CardFactory, MessageFactory, TurnContext } = require('botbuilder');
 process.env["NODE_TLS_REJECT_UNAUTHORIZED"] = 0;
 const { serviceMetadata } = require('../backendModel/metadata');
 
@@ -11,14 +11,15 @@ var password = 'Initial@1'
 class IntentRouter {
 
     constructor() {
-        this.url = 'https://vhcwhbh1ci.cwh.stl.hec.sap.biz:44300/sap/opu/odata/sap/Z630_VA_SRV/ZSDL_CP02_Q003_VAResults?$select=ZPHL3_GRP_T&$format=json';
+        this.url = 'https://vhcwhbh1ci.cwh.stl.hec.sap.biz:44300/sap/opu/odata/sap/Z630_VA_SRV/ZSDL_CP02_Q003_VAResults?$format=json';
     }
 
 
-    fetchProducts(turnContext) {
+    measurebydimension(measure, dimension) {
+        var selectString = '&$select=' + [measure._Name, dimension._Name].join(',');
         return new Promise(function (resolve, reject) {
             request({
-                url: this.url,
+                url: this.url + selectString,
                 auth: {
                     user: username,
                     password: password
@@ -28,11 +29,20 @@ class IntentRouter {
                     var aValues = JSON.parse(body).d.results;
                     var aReplyValues = [];
                     aValues.forEach(function (e) {
-                        aReplyValues.push(e.ZPHL3_GRP_T);
+                        aReplyValues.push(
+                            'For ' + dimension['_sap:label'] + ' ' + e[dimension._Name] + ' \n\nthe ' + measure['_sap:label'] + ' is ' + Math.round(e[measure._Name]*10)/10
+                        );
                     })
+
+                    // var reply = MessageFactory.suggestedActions(aReplyValues, 'Select Product to view further details');
+                    // resolve(aReplyValues.join(','));
                     
-                    var reply = MessageFactory.suggestedActions(aReplyValues, 'Select Product to view further details');
-                    resolve(aReplyValues.join(','));
+                    resolve(MessageFactory.attachment(
+                        CardFactory.heroCard(
+                            aReplyValues.join(',\n'),
+                            ['https://chart.googleapis.com/chart?chxt=x,y&cht=bvs&chd=s:cEj9U&chco=16A4FB&chls=4.0&chs=999x200&chxl=0:|Macintosh|Feb|Mar|Apr|May&chbh=30,70']
+                        )
+                    ));
                 });
         }.bind(this))
     }
